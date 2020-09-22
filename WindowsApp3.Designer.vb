e326ca15518e3d4bc3af4327fd3cd296d224dbb4
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-22 08:53:57 -0600

    Add project files.

---
+++ WindowsApp3.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WindowsApp3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.EmailTextBox = New System.Windows.Forms.TextBox()
+        Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.DisplayButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(45, 41)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(673, 31)
+        Me.FirstNameTextBox.TabIndex = 0
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(45, 102)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(673, 31)
+        Me.LastNameTextBox.TabIndex = 1
+        '
+        'EmailTextBox
+        '
+        Me.EmailTextBox.Location = New System.Drawing.Point(45, 170)
+        Me.EmailTextBox.Name = "EmailTextBox"
+        Me.EmailTextBox.Size = New System.Drawing.Size(673, 31)
+        Me.EmailTextBox.TabIndex = 2
+        '
+        'DisplayLabel
+        '
+        Me.DisplayLabel.Location = New System.Drawing.Point(780, 41)
+        Me.DisplayLabel.Name = "DisplayLabel"
+        Me.DisplayLabel.Size = New System.Drawing.Size(411, 346)
+        Me.DisplayLabel.TabIndex = 3
+        Me.DisplayLabel.Text = "Label1"
+        '
+        'DisplayButton
+        '
+        Me.DisplayButton.Location = New System.Drawing.Point(45, 252)
+        Me.DisplayButton.Name = "DisplayButton"
+        Me.DisplayButton.Size = New System.Drawing.Size(310, 135)
+        Me.DisplayButton.TabIndex = 4
+        Me.DisplayButton.Text = "Display"
+        Me.DisplayButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(408, 252)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(310, 135)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.DisplayButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(1245, 446)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.DisplayButton)
+        Me.Controls.Add(Me.DisplayLabel)
+        Me.Controls.Add(Me.EmailTextBox)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents EmailTextBox As TextBox
+    Friend WithEvents DisplayLabel As Label
+    Friend WithEvents DisplayButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ WindowsApp3.vb
@@ -0,0 +1,26 @@
+﻿Public Class WindowsApp3
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
+        Display()
+    End Sub
+
+    Sub Display()
+        'TODO validate textbox data
+        'MsgBox("Go!")
+        If EmailTextBox.Text = "" Then
+            CreateEmail()
+        End If
+        DisplayLabel.Text = FirstNameTextBox.Text _
+                            & vbNewLine & LastNameTextBox.Text _
+                            & vbNewLine & EmailTextBox.Text
+    End Sub
+
+    Sub CreateEmail()
+        EmailTextBox.Text = FirstNameTextBox.Text _
+                            & "." & LastNameTextBox.Text _
+                            & "@gmail.com"
+    End Sub
+End Class
